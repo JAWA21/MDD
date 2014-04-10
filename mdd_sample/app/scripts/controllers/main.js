6d0a125f634e8f5e4b3a533cb4194c70bf801b11
Jeanna <jawa@fullsail.edu>
2014-04-10 19:39:57 -0400

    tel

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -1,26 +1,17 @@
 var mainControllers = angular.module('mddSampleApp');
-  mainControllers.controller('MainCtrl', function ($scope) {
+    mainControllers.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
-    mainControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-     $http.get('js/data.json').success(function(data) {
-     $scope.artists = data;
-     $scope.artistOrder = 'name';
-  });
   });
 
-    artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
-    $scope.artistOrder = 'name';
-  });
+    mainControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
+     // $http.get('scripts/data.json').success(function(data) {
+     //    console.log(data);
+     // $scope.projects = data;
+     // $scope.projectsOrder = 'name';
+      // console.log($scope.projects);
+    });
 }]);
