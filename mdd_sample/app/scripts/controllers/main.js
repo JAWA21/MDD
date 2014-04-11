448b8e0c4d52d8bb2ff39ceeef9be5e7b0c439c5
Jeanna <jawa@fullsail.edu>
2014-04-10 20:12:11 -0400

    working on the json data

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -8,10 +8,10 @@ var mainControllers = angular.module('mddSampleApp');
   });
 
     mainControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
-     // $http.get('scripts/data.json').success(function(data) {
-     //    console.log(data);
-     // $scope.projects = data;
-     // $scope.projectsOrder = 'name';
-      // console.log($scope.projects);
-    });
+    //  $http.get('scripts/data.json').success(function(data) {
+    //     console.log(data);
+    //  $scope.projects = data;
+    //  $scope.projectsOrder = 'name';
+    //   console.log($scope.projects);
+    // });
 }]);
