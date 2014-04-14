fd5ece9c0c5c7e1796abb84fc7450e856d4659b8
Jeanna <jawa@fullsail.edu>
2014-04-14 14:17:18 -0400

    trying expanded json get method

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -8,10 +8,23 @@ var mainControllers = angular.module('mddSampleApp')
   });
 
     mainControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
-     $http.get('scripts/data.json').success(function(data) {
-        console.log(data);
+    //  $http.get('scripts/data.json').success(function(data) {
+    //     console.log(data);
+    //  $scope.projects = data;
+    //  $scope.projectsOrder = 'name';
+    //   console.log($scope.projects);
+    // });
+
+       $http({method: 'GET', url: 'scripts/data.json'}).
+    success(function(data, status, headers, config) {
+       console.log(data);
      $scope.projects = data;
      $scope.projectsOrder = 'name';
       console.log($scope.projects);
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('error in get json');
     });
 }]);
