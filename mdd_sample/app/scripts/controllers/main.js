a84c6418f3acffca5514dc94511df564baa483b1
Jeanna <jawa@fullsail.edu>
2014-04-14 16:26:32 -0400

    forgot to add controller to html - json working

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -1,6 +1,9 @@
 var mainControllers = angular.module('mddSampleApp')
 
     mainControllers.controller('MainCtrl', function ($scope) {
+
+    $scope.text = 'Hello, Angular fanatic.';
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -9,35 +12,17 @@ var mainControllers = angular.module('mddSampleApp')
   });
 
     mainControllers.controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
-    //  $http.get('scripts/data.json').success(function(data) {
-    //     console.log(data);
-    //  $scope.projects = data;
-    //  $scope.projectsOrder = 'name';
-    //   console.log($scope.projects);
-    // });
 
-     var successCallback function(data){
-        for (var i = data.length - 1; i >= 0; i++) {
-
-            console.log(data[i]);
-        };
-
-     };
-
-     $http.get('../scripts/data.json').success(successCallback);
-
-
-
-    //    $http({method: 'GET', url: 'scripts/data.json'}).
-    // success(function(data, status, headers, config) {
-    //    console.log(data);
-    //  $scope.projects = data;
-    //  $scope.projectsOrder = 'name';
-    //   console.log($scope.projects);
-    // }).
-    // error(function(data, status, headers, config) {
-    //   // called asynchronously if an error occurs
-    //   // or server returns response with an error status.
-    //   console.log('error in get json');
-    // });
+       $http({method: 'GET', url: 'scripts/data.json'}).
+    success(function(data, status, headers, config) {
+       console.log(data);
+     $scope.projects = data;
+     $scope.projectsOrder = 'name';
+      console.log($scope.projects);
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('error in get json');
+    });
 }]);
