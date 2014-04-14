c5f1f92731595a264377f83ce031e4361fcfb52a
Jeanna <jawa@fullsail.edu>
2014-04-14 14:19:45 -0400

    parse error

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -1,4 +1,4 @@
-var mainControllers = angular.module('mddSampleApp')
+var mainControllers = angular.module('mddSampleApp');
     mainControllers.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
