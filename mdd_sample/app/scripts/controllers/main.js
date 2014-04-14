ab62c5519bc81857675a6dee177d6b8aff38ba58
Jeanna <jawa@fullsail.edu>
2014-04-14 14:29:06 -0400

    maybe?

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -15,13 +15,13 @@ var mainControllers = angular.module('mddSampleApp');
     //   console.log($scope.projects);
     // });
 
-     var successCallback(function(data){
+     var successCallback function(data){
         for (var i = data.length - 1; i >= 0; i++) {
 
             console.log(data[i]);
         };
 
-     });
+     };
 
      $http.get('../scripts/data.json').success(successCallback);
 
