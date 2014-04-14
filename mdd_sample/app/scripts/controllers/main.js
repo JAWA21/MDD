8c3376e1cb356fdf90663e375142894ab3d0930a
Jeanna <jawa@fullsail.edu>
2014-04-14 14:26:28 -0400

    trying different method

--- mdd_sample/app/scripts/controllers/main.js
+++ mdd_sample/app/scripts/controllers/main.js
@@ -15,18 +15,26 @@ var mainControllers = angular.module('mddSampleApp');
     //   console.log($scope.projects);
     // });
 
-     //$http.get('../scripts/data.json').success(successCallback);
+     $http.get('../scripts/data.json').success(successCallback);
 
-       $http({method: 'GET', url: '../scripts/data.json'}).
-    success(function(data, status, headers, config) {
-       console.log(data);
-     $scope.projects = data;
-     $scope.projectsOrder = 'name';
-      console.log($scope.projects);
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      console.log('error in get json');
-    });
+     successCallback(function(data){
+        for (var i = data.length - 1; i >= 0; i++) {
+
+            console.log(data[i]);
+        };
+
+     });
+
+    //    $http({method: 'GET', url: 'scripts/data.json'}).
+    // success(function(data, status, headers, config) {
+    //    console.log(data);
+    //  $scope.projects = data;
+    //  $scope.projectsOrder = 'name';
+    //   console.log($scope.projects);
+    // }).
+    // error(function(data, status, headers, config) {
+    //   // called asynchronously if an error occurs
+    //   // or server returns response with an error status.
+    //   console.log('error in get json');
+    // });
 }]);
